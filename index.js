a25faf1e3b0d9d90d3a6536edfbf1aa5ec1c3245
Randall Degges <r@rdegges.com>
2015-08-28 16:04:56 -0700

    Adding some codez.

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+var cheerio = require('cheerio');
+var request = require('request');
+
+var TPB_URL = 'https://thepiratebay.la/top/207';
+
+request(TPB_URL, function(err, resp, body) {
+  if (err || resp.statusCode !== 200) {
+    throw new Error("Couldn't reach thepiratebay... It might be down :(");
+  }
+
+  console.log('Top 100 Movies from The Pirate Bay');
+  console.log('----------------------------------\n');
+
+  var $ = cheerio.load(body);
+  $('#content .detName a').each(function(index, element) {
+    console.log(index, $(this).text());
+  });
+});
