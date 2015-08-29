3b917d04b266f1c2ec77dd40d16fa0b6acd3c3e5
Samantha <samanthadegges@gmail.com>
2015-08-29 12:11:39 -0700

    Update index.js
    Fixed typo for ya!

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var TPB_URL = 'https://thepiratebay.la/top/207';
 
 request(TPB_URL, function(err, resp, body) {
   if (err || resp.statusCode !== 200) {
-    throw new Error("Couldn't reach thepiratebay... It might be down :(");
+    throw new Error("Couldn't reach The Pirate Bay... It might be down :(");
   }
 
   console.log('Top 100 Movies from The Pirate Bay');
